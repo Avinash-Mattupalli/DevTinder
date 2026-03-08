ad54a6298bb3c3f5540e717e332550c1308f655c
Avinash Mattupalli <avinashmattupalli@gmail.com>
2026-03-08 18:27:50 +0530

    playing with routes

--- src/app.js
+++ src/app.js
@@ -5,6 +5,23 @@ app.listen(7777, () => {
   console.log("Server Running Successfully On Port 7777");
 });
 
+app.get("/user/:userId/:name/:pwd", (req, res) => {
+  console.log(req.query);
+  res.send({ firstname: "Avinash", lastname: "Mattupalli" });
+});
+app.get("/user/:userId/:name/:pwd", (req, res) => {
+  console.log(req.params);
+  res.send({ firstname: "Avinash", lastname: "Mattupalli" });
+});
+app.get("/user", (req, res) => {
+  res.send({ firstname: "Avinash", lastname: "Mattupalli" });
+});
+app.post("/user", (req, res) => {
+  res.send("Data successfully stored in DB");
+});
+app.delete("/user", (req, res) => {
+  res.send("Data deleted in DB");
+});
 app.use("/login", (req, res) => {
   res.send("DevTinder Login Page");
 });
