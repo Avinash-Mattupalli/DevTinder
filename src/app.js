eeaec0cf27987151bc3ee525ecf2fe775bf6aae2
Avinash Mattupalli <avinashmattupalli@gmail.com>
2026-03-10 23:03:42 +0530

    inital db

--- src/app.js
+++ src/app.js
@@ -1,82 +1,103 @@
 const express = require("express");
 const { admin_auth } = require("./middlewares/adminAuth");
+const User = require("./models/user");
+const User = require("./models/user");
 const app = express();
 
 app.listen(7777, () => {
   console.log("Server Running Successfully On Port 7777");
 });
 
-app.get("/user/:userId/:name/:pwd", (req, res) => {
-  console.log(req.query);
-  res.send({ firstname: "Avinash", lastname: "Mattupalli" });
-});
-app.get("/user/:userId/:name/:pwd", (req, res) => {
-  console.log(req.params);
-  res.send({ firstname: "Avinash", lastname: "Mattupalli" });
-});
-app.get("/user", (req, res) => {
-  res.send({ firstname: "Avinash", lastname: "Mattupalli" });
-});
-app.post("/user", (req, res) => {
-  res.send("Data successfully stored in DB");
-});
-app.delete("/user", (req, res) => {
-  res.send("Data deleted in DB");
-});
-app.use("/login", (req, res) => {
-  res.send("DevTinder Login Page");
-});
-app.use("/requests", (req, res) => {
-  res.send("DevTinder Requests Page");
-});
-app.use("/profile", (req, res) => {
-  res.send("DevTinder Profile Page");
-});
-app.use("/", (req, res) => {
-  res.send("DevTinder Home Page");
-});
+app.post("/signup", async (req, res, next) => {
+  const userObj = {
+    firstName: "Avinash",
+    lastName: "Mattupalli",
+    email: "avinash@gmail.com",
+    password: "avinash123",
+    age: 24,
+    gender: "Male",
+  };
+  const user = new User(userObj);
 
-//Example of next to call the next available handler
-app.use(
-  "/user",
-  (req, res, next) => {
-    console.log("Handler 1");
-    res.send("Handler 1 executed");
-    next();
-  },
-  (req, res) => {
-    console.log("Handler 2");
-    res.send("Handler 2 executed");
-  },
-);
-// even handlers are wrapped in an array it behaves same as above. all or some handlers can be wrapped in an array. can be used in any CURD methods
-app.use("/user", [
-  (req, res, next) => {
-    console.log("Handler 1");
-    res.send("Handler 1 executed");
-    next();
-  },
-  (req, res) => {
-    console.log("Handler 2");
-    res.send("Handler 2 executed");
-  },
-]);
-
-// auth example
-
-app.get("/admin/getall", admin_auth);
-
-// error handling
-app.use("/", (err, req, res, next) => {
-  if (err) {
-    res.status(500).send("Something went wrong");
-  }
-});
-// error handling using try catch
-app.get("/userdata", (req, res, next) => {
   try {
-    throw new Error("Error Test");
+    await user.save();
+    res.send("User Added Successfully");
   } catch (err) {
-    res.status(500).send("error occured");
+    res.status(400).send("Error saving data: " + err.message);
   }
 });
+
+// app.get("/user/:userId/:name/:pwd", (req, res) => {
+//   console.log(req.query);
+//   res.send({ firstname: "Avinash", lastname: "Mattupalli" });
+// });
+// app.get("/user/:userId/:name/:pwd", (req, res) => {
+//   console.log(req.params);
+//   res.send({ firstname: "Avinash", lastname: "Mattupalli" });
+// });
+// app.get("/user", (req, res) => {
+//   res.send({ firstname: "Avinash", lastname: "Mattupalli" });
+// });
+// app.post("/user", (req, res) => {
+//   res.send("Data successfully stored in DB");
+// });
+// app.delete("/user", (req, res) => {
+//   res.send("Data deleted in DB");
+// });
+// app.use("/login", (req, res) => {
+//   res.send("DevTinder Login Page");
+// });
+// app.use("/requests", (req, res) => {
+//   res.send("DevTinder Requests Page");
+// });
+// app.use("/profile", (req, res) => {
+//   res.send("DevTinder Profile Page");
+// });
+// app.use("/", (req, res) => {
+//   res.send("DevTinder Home Page");
+// });
+
+// //Example of next to call the next available handler
+// app.use(
+//   "/user",
+//   (req, res, next) => {
+//     console.log("Handler 1");
+//     res.send("Handler 1 executed");
+//     next();
+//   },
+//   (req, res) => {
+//     console.log("Handler 2");
+//     res.send("Handler 2 executed");
+//   },
+// );
+// // even handlers are wrapped in an array it behaves same as above. all or some handlers can be wrapped in an array. can be used in any CURD methods
+// app.use("/user", [
+//   (req, res, next) => {
+//     console.log("Handler 1");
+//     res.send("Handler 1 executed");
+//     next();
+//   },
+//   (req, res) => {
+//     console.log("Handler 2");
+//     res.send("Handler 2 executed");
+//   },
+// ]);
+
+// // auth example
+
+// app.get("/admin/getall", admin_auth);
+
+// // error handling
+// app.use("/", (err, req, res, next) => {
+//   if (err) {
+//     res.status(500).send("Something went wrong");
+//   }
+// });
+// // error handling using try catch
+// app.get("/userdata", (req, res, next) => {
+//   try {
+//     throw new Error("Error Test");
+//   } catch (err) {
+//     res.status(500).send("error occured");
+//   }
+// });
